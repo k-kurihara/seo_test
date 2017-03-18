6049d7c5c566ef00c0ed6740a6dbc3077d85cf2b
kurihara_kazuyoshi <kazu@kurihara-kazuyoshi-no-MacBook-Air.local>
2017-03-18 18:39:21 +0900

    dataをhtmlにappend

---
+++ htdocs/assets/js/common.js
@@ -0,0 +1,18 @@
+
+$(function(){
+  $.ajax({
+    type: 'GET',
+    url: 'http://beta.json-generator.com/api/json/get/41uVeJUsf',
+    dataType: 'json',
+    success: function(json){
+      var len = json.length;
+      for(var i=0; i < len; i++){
+        var data = json[i];
+        var title = data.title;
+        var desc = data.desc;
+        $('.jsc_data_title').append(title)
+        $('.jsc_data_desc').append(desc)
+      }
+    }
+  });
+})
