d355c99e8b9eae1e7de1ac14c253e031b65549bc
kazuyoshi.kurihara <kazuyoshi.kurihara@i-studio.co.jp>
2017-03-21 11:49:30 +0900

    DOMの構成変更

--- htdocs/assets/js/common.js
+++ htdocs/assets/js/common.js
@@ -1,18 +1,22 @@
 
 $(function(){
-  $.ajax({
-    type: 'GET',
-    url: 'json/index.json',
-    dataType: 'json',
-    success: function(json){
-      var len = json.length;
-      for(var i=0; i < len; i++){
-        var data = json[i];
-        var title = data.title;
-        var desc = data.desc;
-        $('.jsc_data_title').append(title)
-        $('.jsc_data_desc').append(desc)
+    $.ajax({
+      type: 'GET',
+      url: 'json/index.json',
+      dataType: 'json',
+      success: function(json){
+        var len = json.length;
+        for(var i=0; i < len; i++){
+          var data = json[i];
+          var title = data.title;
+          var desc = data.desc;
+
+          var titleDOM = '<h2 class=".jsc_data_title">' + title + '</h2>'
+          var descDOM = '<div class=".jsc_data_desc">' + desc + '</div>'
+
+          $('.jsc_data_push').append(titleDOM).append(descDOM)
+
+        }
       }
-    }
-  });
+    });
 })
